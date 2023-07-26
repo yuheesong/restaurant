7666b727d0c2a47095f34e7f1c664d01714bc4ec
yuheesong <thddbgml345@naver.com>
2023-07-26 18:18:00 +0900

    쿼리dsl 세팅2

--- src/main/java/com/restaurant/repository/RestRepository.java
+++ src/main/java/com/restaurant/repository/RestRepository.java
@@ -2,10 +2,11 @@ package com.restaurant.repository;
 
 import com.restaurant.entity.Rest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface RestRepository extends JpaRepository<Rest, Long> {
+public interface RestRepository extends JpaRepository<Rest, Long>, QuerydslPredicateExecutor<Rest> {
 
     List<Rest> findByRestNm(String restNm);
 
