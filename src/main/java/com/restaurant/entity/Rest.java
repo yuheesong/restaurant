280ded34dc34d2a28974b3eb3586faa4c5ce70e0
yuheesong <thddbgml345@naver.com>
2023-07-26 18:05:30 +0900

    레스토랑 엔티티

---
+++ src/main/java/com/restaurant/entity/Rest.java
@@ -0,0 +1,39 @@
+package com.restaurant.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="rest")
+@Getter
+@Setter
+@ToString
+public class Rest {
+    @Id
+    @Column(name="rest_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String restNm;
+
+    @Column(name = "restPhone", nullable = false)
+    private int restPhone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Lob
+    @Column(nullable = false)
+    private String introduction;
+
+    @Lob
+    @Column(nullable = false)
+    private String restDetail;
+
+    @Column(nullable = false)
+    private String category;
+}
---
+++ src/main/java/com/restaurant/repository/RestRepository.java
@@ -0,0 +1,4 @@
+package com.restaurant.repository;
+
+public class RestRepository {
+}
