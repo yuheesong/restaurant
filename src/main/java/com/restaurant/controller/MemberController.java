0d415251e316159519c1d1ec0e5c2b9d295f5c8d
yuheesong <thddbgml345@naver.com>
2023-08-29 21:21:37 +0900

    프론트 - 회원가입 페이지

--- src/main/java/com/restaurant/controller/MemberController.java
+++ src/main/java/com/restaurant/controller/MemberController.java
@@ -79,7 +79,7 @@ public class MemberController {
 
     @GetMapping(value = "/login/error")
     public String loginError(Model model){
-        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요.");
         return "/member/memberLoginForm";
     }
 
